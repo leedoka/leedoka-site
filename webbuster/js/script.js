683e38f3108db1fabaf945171ac3d42147bb6f3e
leedoka <108570824+leedoka@users.noreply.github.com>
2022-11-06 21:33:13 +0300

    Add files via upload

---
+++ webbuster/js/script.js
@@ -0,0 +1,59 @@
+const buyCoffeeButtons = document.querySelectorAll('.btn__to__buy');
+
+const closeModalWindow = document.getElementById('remodal__close');
+const modalWindow = document.querySelector('.remodal');
+const productField = document.getElementById('product-name');
+
+// buyCoffee.onclick = function () {
+//   modalWindow.classList.remove('invisible');
+// };
+
+buyCoffeeButtons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    const productName = button.dataset.productName;
+    productField.value = productName;
+    modalWindow.classList.remove('invisible');
+  })
+})
+
+closeModalWindow.onclick = function () {
+  modalWindow.classList.add('invisible');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function () {
+//     $("form").submit(function () {
+//         // Получение ID формы
+//         var formID = $(this).attr('id');
+//         // Добавление решётки к имени ID
+//         var formNm = $('#' + formID);
+//         $.ajax({
+//             type: "POST",
+//             url: 'mail.php',
+//             data: formNm.serialize(),
+//             success: function (data) {
+//                 // Вывод текста результата отправки
+//                 $(formNm).html(data);
+//             },
+//             error: function (jqXHR, text, error) {
+//                 // Вывод текста ошибки отправки
+//                 $(formNm).html(error);
+//             }
+//         });
+//         return false;
+//     });
+// });
